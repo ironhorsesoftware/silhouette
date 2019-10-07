ee858aa383b031010d5a63de2c148576d8b5e48b
Mike Pigott <mpigott@ironhorsesoftware.tech>
2019-10-07 13:52:49 -0400

    Adding an e-mail address to the LinkedIn provider.

--- src/main/scala/com/ironhorsesoftware/play/silhouette/impl/providers/oauth2/LinkedInProvider.scala
+++ src/main/scala/com/ironhorsesoftware/play/silhouette/impl/providers/oauth2/LinkedInProvider.scala
@@ -1,6 +1,6 @@
 package com.ironhorsesoftware.play.silhouette.impl.providers.oauth2
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 import com.mohiva.play.silhouette.api.LoginInfo
 import com.mohiva.play.silhouette.api.util.HTTPLayer
@@ -22,22 +22,28 @@ class LinkedInProvider(
 
   override val id = LinkedInProvider.ID
 
-  override protected val urls = Map("api" -> settings.apiURL.getOrElse(LinkedInProvider.API))
+  override protected val urls =
+    Map("api" -> settings.apiURL.getOrElse(LinkedInProvider.API),
+        "email" -> settings.customProperties.getOrElse("emailUrl", LinkedInProvider.EMAIL_URL))
 
   override val profileParser = new LinkedInProfileParser
 
   override protected def buildProfile(authInfo: OAuth2Info): Future[Profile] = {
+
+    val emailAddrOptFuture =
+      httpLayer.url(urls("email")).withHttpHeaders(("Authorization", s"Bearer ${authInfo.accessToken}")).get().flatMap { response =>
+        val json = response.json
+        (json \ "serviceErrorCode").asOpt[Int] match {
+          case Some(errorCode) => Future.failed(buildError(json, errorCode))
+          case _ => Future.successful(findEmailAddress(json))
+        }
+      }
+
     httpLayer.url(urls("api")).withHttpHeaders(("Authorization", s"Bearer ${authInfo.accessToken}")).get().flatMap { response =>
       val json = response.json
       (json \ "serviceErrorCode").asOpt[Int] match {
-        case Some(error) =>
-          val message = (json \ "message").asOpt[String]
-          val requestId = (json \ "requestId").asOpt[String]
-          val status = (json \ "status").asOpt[Int]
-          val timestamp = (json \ "timestamp").asOpt[Long]
-
-          Future.failed(new ProfileRetrievalException(SpecifiedProfileError.format(id, error, message, requestId, status, timestamp)))
-        case _ => profileParser.parse(json, authInfo)
+        case Some(errorCode) => Future.failed(buildError(json, errorCode))
+        case _ => profileParser.parseWithEmailAddress(json, emailAddrOptFuture, authInfo)
       }
     }
   }
@@ -45,20 +51,28 @@ class LinkedInProvider(
   override def withSettings(f : (Settings) => Settings) = {
     new LinkedInProvider(httpLayer, stateHandler, f(settings))
   }
+
+  private def buildError(json : JsValue, errorCode : Int) : ProfileRetrievalException = {
+    val message = (json \ "message").asOpt[String]
+    val requestId = (json \ "requestId").asOpt[String]
+    val status = (json \ "status").asOpt[Int]
+    val timestamp = (json \ "timestamp").asOpt[Long]
+
+    new ProfileRetrievalException(SpecifiedProfileError.format(id, errorCode, message, requestId, status, timestamp))
+  }
+
+  private def findEmailAddress(json : JsValue) : Option[String] = {
+    (json \ "handle~" \ "emailAddress").asOpt[String].filter(item => !item.isEmpty)
+  }
 }
 
 class LinkedInProfileParser extends SocialProfileParser[JsValue, CommonSocialProfile, OAuth2Info] {
   override def parse(json : JsValue, authInfo: OAuth2Info) = Future.successful {
-    parseCommonSocialProfile(json, authInfo)
-  }
-
-  def parseCommonSocialProfile(json : JsValue, authInfo : OAuth2Info) : CommonSocialProfile = {
     val userID = (json \ "id").as[String]
     val firstName = (json \ "localizedFirstName").asOpt[String].filter(item => !item.isEmpty)
     val lastName = (json \ "localizedLastName").asOpt[String].filter(item => !item.isEmpty)
     val fullName = (json \ "vanityName").asOpt[String].filter(item => !item.isEmpty)
     val avatarURL = findAvatarUrl(json)
-    val email = (json \ "emailAddress").asOpt[String].filter(item => !item.isEmpty)
 
     CommonSocialProfile(
       loginInfo = LoginInfo(LinkedInProvider.ID, userID),
@@ -66,7 +80,33 @@ class LinkedInProfileParser extends SocialProfileParser[JsValue, CommonSocialPro
       lastName = lastName,
       fullName = fullName,
       avatarURL = avatarURL,
-      email = email)    
+      email = None)
+  }
+
+  def parseWithEmailAddress(json : JsValue, emailAddrOptFuture : Future[Option[String]], authInfo : OAuth2Info)(implicit ec : ExecutionContext) : Future[CommonSocialProfile] = {
+    emailAddrOptFuture.flatMap { email =>
+      parse(json, authInfo).map { profile =>
+        profile.copy(email = email)
+      }
+    }
+  }
+
+  def parseCommonSocialProfile(json : JsValue, emailAddrOptFuture : Future[Option[String]], authInfo : OAuth2Info)(implicit ec : ExecutionContext) : Future[CommonSocialProfile] = {
+    val userID = (json \ "id").as[String]
+    val firstName = (json \ "localizedFirstName").asOpt[String].filter(item => !item.isEmpty)
+    val lastName = (json \ "localizedLastName").asOpt[String].filter(item => !item.isEmpty)
+    val fullName = (json \ "vanityName").asOpt[String].filter(item => !item.isEmpty)
+    val avatarURL = findAvatarUrl(json)
+
+    emailAddrOptFuture.map { email =>
+      CommonSocialProfile(
+        loginInfo = LoginInfo(LinkedInProvider.ID, userID),
+        firstName = firstName,
+        lastName = lastName,
+        fullName = fullName,
+        avatarURL = avatarURL,
+        email = email)
+    }
   }
 
   def findAvatarUrl(json : JsValue) : Option[String] = {
@@ -86,4 +126,5 @@ object LinkedInProvider {
   val ID = com.mohiva.play.silhouette.impl.providers.oauth2.LinkedInProvider.ID
 
   val API = "https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName,localizedHeadline,vanityName,profilePicture(displayImage~playableStreams))"
+  val EMAIL_URL = "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))"
 }
