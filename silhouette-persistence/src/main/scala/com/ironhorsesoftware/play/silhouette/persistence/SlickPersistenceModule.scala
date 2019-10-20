92c16a2cdd8df85ef74e6067f85d2eae40cc74f9
Mike Pigott <mpigott@ironhorsesoftware.tech>
2019-10-20 18:23:42 -0400

    Wiring in the Google TOTP DAO into the module repository, and adding helper methods for OpenID authentication.

--- silhouette-persistence/src/main/scala/com/ironhorsesoftware/play/silhouette/persistence/SlickPersistenceModule.scala
+++ silhouette-persistence/src/main/scala/com/ironhorsesoftware/play/silhouette/persistence/SlickPersistenceModule.scala
@@ -8,13 +8,13 @@ import net.codingwell.scalaguice.ScalaModule
 
 import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
 import com.mohiva.play.silhouette.api.util.PasswordInfo
-import com.mohiva.play.silhouette.impl.providers.{OAuth1Info, OAuth2Info, OpenIDInfo, CasInfo}
+import com.mohiva.play.silhouette.impl.providers.{OAuth1Info, OAuth2Info, OpenIDInfo, CasInfo, GoogleTotpInfo}
 import com.mohiva.play.silhouette.persistence.daos.DelegableAuthInfoDAO
 import com.mohiva.play.silhouette.persistence.repositories.DelegableAuthInfoRepository
 
 import com.ironhorsesoftware.play.silhouette.persistence.daos.{SlickCasInfoDAO, SlickPasswordInfoDAO}
 import com.ironhorsesoftware.play.silhouette.persistence.daos.{SlickOAuth1InfoDAO, SlickOAuth2InfoDAO}
-import com.ironhorsesoftware.play.silhouette.persistence.daos.SlickOpenIdDAO
+import com.ironhorsesoftware.play.silhouette.persistence.daos.{SlickOpenIdDAO, SlickGoogleTotpInfoDAO}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -26,6 +26,7 @@ class SlickPersistenceModule @Inject() extends AbstractModule with ScalaModule {
     bind[DelegableAuthInfoDAO[OAuth1Info]].to[SlickOAuth1InfoDAO]
     bind[DelegableAuthInfoDAO[OAuth2Info]].to[SlickOAuth2InfoDAO]
     bind[DelegableAuthInfoDAO[OpenIDInfo]].to[SlickOpenIdDAO]
+    bind[DelegableAuthInfoDAO[GoogleTotpInfo]].to[SlickGoogleTotpInfoDAO]
   }
 
   @Provides
@@ -34,8 +35,9 @@ class SlickPersistenceModule @Inject() extends AbstractModule with ScalaModule {
       passwordInfoDAO : DelegableAuthInfoDAO[PasswordInfo],
       oauth1InfoDAO : DelegableAuthInfoDAO[OAuth1Info],
       oauth2InfoDAO : DelegableAuthInfoDAO[OAuth2Info],
-      openIdInfoDAO : DelegableAuthInfoDAO[OpenIDInfo]) : AuthInfoRepository = {
+      openIdInfoDAO : DelegableAuthInfoDAO[OpenIDInfo],
+      googleTotpInfoDAO : DelegableAuthInfoDAO[GoogleTotpInfo]) : AuthInfoRepository = {
 
-    new DelegableAuthInfoRepository(casInfoDAO, passwordInfoDAO, oauth1InfoDAO, oauth2InfoDAO, openIdInfoDAO)
+    new DelegableAuthInfoRepository(casInfoDAO, passwordInfoDAO, oauth1InfoDAO, oauth2InfoDAO, openIdInfoDAO, googleTotpInfoDAO)
   }
 }
--- silhouette-persistence/src/main/scala/com/ironhorsesoftware/play/silhouette/persistence/model/OpenIdCredentials.scala
+++ silhouette-persistence/src/main/scala/com/ironhorsesoftware/play/silhouette/persistence/model/OpenIdCredentials.scala
@@ -6,3 +6,13 @@ import com.mohiva.play.silhouette.impl.providers.OpenIDInfo
 case class OpenIdCredentials(id : Int, providerId : String, providerKey : String, openId : String) {
   def loginInfo = LoginInfo(providerId, providerKey)
 }
+
+object OpenIdCredentials extends Function4[Int, String, String, String, OpenIdCredentials] {
+  def apply(loginInfo : LoginInfo, authInfo : OpenIDInfo) : OpenIdCredentials = {
+    OpenIdCredentials(0, loginInfo.providerID, loginInfo.providerKey, authInfo.id)
+  }
+
+  def buildAuthInfo(credentials: OpenIdCredentials, attributes : Seq[OpenIdAttribute]) = {
+    OpenIDInfo(credentials.openId, attributes.map(attr => attr.attribute).toMap)
+  }
+}
